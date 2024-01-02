50dc1e1e79ee8030fd616ab8028e454390cf0214
Gongchr <gchrvito+01@gmail.com>
2024-01-02 22:19:44 +0800

    Create qm.js

---
+++ rewrite/qm.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+https://api\.qingmang\.mobi/v2/publication\.member\.info url script-response-body https://raw.githubusercontent.com/gongchr/quanx/main/qm.js
+
+[mitm] 
+hostname = api.qingmang.mobi
