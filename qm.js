16056ceb124de1022af0c82b642afe71bf3721e1
Gongchr <gchrvito+01@gmail.com>
2024-01-02 22:34:18 +0800

    Update qm.js

--- qm.js
+++ qm.js
@@ -7,6 +7,21 @@ if (url.includes("/v2/publication.member.info")) {
     body = body.replace(/"expiredTimestamp"\s*:\s*\d+/g, '"expiredTimestamp": 4102545599000');
     body = body.replace(/"vipExpiredTime"\s*:\s*\d+/g, '"vipExpiredTime": 4102545599000');
     body = body.replace(/"isVip"\s*:\s*(true|false)/g, '"isVip": true');
+
+    body = body.replace(/"type"\s*:\s*"stranger"/g, '"type": "normal"');
+
+    let jsonObj = JSON.parse(body);
+
+    if (!jsonObj.profile.hasOwnProperty('isVip')) {
+      jsonObj.profile.isVip = true;
+    }
+    if (!jsonObj.profile.role.hasOwnProperty('expiredTimestamp')) {
+      jsonObj.profile.role.expiredTimestamp = 4102545599000;
+    }
+    if (!jsonObj.profile.hasOwnProperty('vipExpiredTime')) {
+      jsonObj.profile.vipExpiredTime = 4102545599000;
+    }
+    body = JSON.stringify(jsonObj);
 }
 
 $done({ body });
